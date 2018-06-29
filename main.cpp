ab16ec73914c4805f363378ffca4a00b63f4d391
madebymuiz <muiz.reform@gmail.com>
2018-06-29 16:59:04 -0400

    Added vector print function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 
-template <class T> print_vec (std::vector<T>* vec) {
+template <class T>
+void print_vec (std::vector<T>* vec) {
   std::cout << "vec: ";
   for ( unsigned int i = 0; i < vec->size(); i++ ) {
     std::cout << (*vec)[i] << " ";
@@ -11,12 +12,20 @@ template <class T> print_vec (std::vector<T>* vec) {
 
 int main () {
 
+  std::cout << "Begin" << std::endl;
   std::vector<double> v(4, 0.0); // vector with 4 allocations of 0.0
   v[0] = 13.1; v[2] = 3.14;
+
+  print_vec(&v);
 
   std::vector<double> u(v); // copy v into vector u
   u[2] = 6.5;
   u[3] = -4.8;
+
+  std::cout << "----------" << std::endl;
+  std::cout << "After" << std::endl;
+  print_vec(&v);
+  print_vec(&u);
 
   return 0;
 }
