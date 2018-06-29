610ee6af84686fc1f3a2420788e67d88bbf7ab7d
ClayMason <muiz.project@gmail.com>
2018-06-29 18:20:44 -0400

    Added assignment operator overloading for vector class

--- vec.cpp
+++ vec.cpp
@@ -16,5 +16,29 @@ void Vec<T>::create () {
 */
 template <class T>
 void Vec<T>::create (size_type n, const T& val) {
+  this->m_data = new T[n];
+  this->m_size = this->m_alloc = n;
 
+  // storing the value of class T to populate the vector
+  for ( size_type i = 0; i < this->m_size; i++ )
+    this->m_data[i] = val;
+
+}
+
+/*
+ * Assignment Operator Overloading
+ * Given a const vector instance v, return a copy of the vector
+ * and assign it to the l-value instance
+*/
+template <class T>
+Vec<T>& Vec<T>::operator= (const Vec<T>& v) {
+  // only assign if this vector is a different vector
+  if ( this != &v ) {
+    // delete data in current vector and copy the new vector into this vector
+    delete [] m_data;
+    this->copy(v);
+  }
+
+  // return the copy vector, which should be this
+  return *this;
 }
